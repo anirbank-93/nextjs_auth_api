09553d5e43eb86edff2aa0d0b2546d7cdfe69ba5
Anirban Karmakar <anirbankarmakar0190@gmail.com>
2024-11-30 23:16:16 +0530

    Update user by id

---
+++ app/api/(auth)/users/[id]/route.ts
@@ -0,0 +1,48 @@
+import connect from "@/lib/db";
+import userModel from "@/lib/models/users.model";
+import { NextResponse } from "next/server";
+import mongoose from "mongoose";
+
+export const PATCH = async (
+  request: Request,
+  route: { params: { id: mongoose.Types.ObjectId } }
+) => {
+  const { id } = await route.params;
+
+  try {
+    const body = await request.json();
+    await connect();
+
+    if (!id || !body || Object.keys(body).length === 0) {
+      return new NextResponse(
+        JSON.stringify({ message: "ID or data not found" }),
+        { status: 400 }
+      );
+    }
+
+    if (!mongoose.Types.ObjectId.isValid(id)) {
+      return new NextResponse(JSON.stringify({ message: "Invalid ID." }), {
+        status: 400,
+      });
+    }
+
+    const user = await userModel.findOneAndUpdate({ _id: id }, body, {
+      new: true,
+    });
+
+    if (!user) {
+      return new NextResponse(JSON.stringify({ message: "User not found." }), {
+        status: 404,
+      });
+    }
+
+    return new NextResponse(
+      JSON.stringify({ message: "User updated", data: user }),
+      { status: 200 }
+    );
+  } catch (error: any) {
+    return new NextResponse("Failed to update user due to " + error.message, {
+      status: 500,
+    });
+  }
+};
