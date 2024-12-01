5d37129dd26d7c1fce049ea9c6ed44388ded2c20
Anirban Karmakar <anirbankarmakar0190@gmail.com>
2024-12-01 20:23:21 +0530

    Get all blog categories route

--- app/api/(dashboard)/category/route.ts
+++ app/api/(dashboard)/category/route.ts
@@ -2,3 +2,11 @@ import connect from "@/lib/db";
 import userModel from "@/lib/models/users.model";
 import { NextResponse } from "next/server";
 import categoryModel from "@/lib/models/category.model";
+
+export const GET = async (request:Request) => {
+    try {
+        //
+    } catch (error:any) {
+        //
+    }
+}
